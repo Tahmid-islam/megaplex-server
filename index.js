fe98e707223ad5615e7071e0436df8c8a0fad36c
Md. Ahanaf Tahmid Islame <tahmid231@gmail.com>
2021-12-19 19:14:37 +0600

    All Complete

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nsljh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
+console.log(uri);
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -21,9 +22,146 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
-    const database = client.db("classicWatch");
-    const productsCollection = database.collection("products");
-    console.log("Database Connected Successfully");
+    const database = client.db("megaplex");
+    const productCollection = database.collection("movies");
+    const bookingCollection = database.collection("bookings");
+    const userCollection = database.collection("users");
+    const reviewCollection = database.collection("reviews");
+
+    // Post movie API
+    app.post("/movies", async (req, res) => {
+      const product = req.body;
+      const result = await productCollection.insertOne(product);
+      res.json(result);
+    });
+
+    // Get movies API
+    app.get("/movies", async (req, res) => {
+      const cursor = productCollection.find({});
+      const size = parseInt(req.query.size);
+      let movies;
+      if (size) {
+        movies = await cursor.limit(size).toArray();
+      } else {
+        movies = await cursor.toArray();
+      }
+      res.send({
+        movies,
+      });
+    });
+
+    // Get Single movies API
+    app.get("/movies/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await productCollection.findOne(query);
+      res.json(service);
+    });
+
+    // Delete Single Product API
+    app.delete("/movies/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productCollection.deleteOne(query);
+      res.json(result);
+    });
+
+    // Post user information tp database after register new user
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      res.json(result);
+    });
+
+    // Update user information in database if user exists or not in google log in
+    app.put("/users", async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      res.json(result);
+    });
+
+    // Post order information to database
+    app.post("/bookings", async (req, res) => {
+      const bookings = req.body;
+      const result = await bookingCollection.insertOne(bookings);
+      res.json(result);
+    });
+
+    // Get all orders API
+    app.get("/bookings", async (req, res) => {
+      const cursor = bookingCollection.find({});
+      const booking = await cursor.toArray();
+      res.send(booking);
+    });
+
+    // Delete  Orders API
+    app.delete("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await bookingCollection.deleteOne(query);
+      res.json(result);
+    });
+
+    // Get specific user order API
+    app.get("/myBookings/:email", async (req, res) => {
+      const result = await bookingCollection
+        .find({
+          email: req.params.email,
+        })
+        .toArray();
+      res.send(result);
+    });
+
+    // Status update
+    app.put("/updateStatus/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const result = await bookingCollection.updateOne(filter, {
+        $set: {
+          status: "Confirmed",
+        },
+      });
+      res.send(result);
+    });
+
+    // Post Review API
+    app.post("/reviews", async (req, res) => {
+      const review = req.body;
+      const result = await reviewCollection.insertOne(review);
+      res.json(result);
+    });
+
+    // Get all reviews API
+    app.get("/reviews", async (req, res) => {
+      const cursor = reviewCollection.find({});
+      const reviews = await cursor.toArray();
+      res.send(reviews);
+    });
+
+    // Make admin role API
+    app.put("/admin", async (req, res) => {
+      const filter = { email: req.body.email };
+      const result = await userCollection.find(filter).toArray();
+      if (result) {
+        const documents = await userCollection.updateOne(filter, {
+          $set: { role: "admin" },
+        });
+        res.json(documents);
+      }
+    });
+
+    // check admin or not
+    app.get("/admin/:email", async (req, res) => {
+      const result = await userCollection
+        .find({ email: req.params.email })
+        .toArray();
+      res.send(result);
+    });
   } finally {
     // await client.close();
   }
@@ -31,9 +169,9 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Megaplex Server Running");
 });
 
 app.listen(port, () => {
-  console.log(`Classic Watch Server Running On Port`, port);
+  console.log(`Megaplex Server Running On Port`, port);
 });
